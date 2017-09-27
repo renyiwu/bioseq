d5f397f500397a43d87a712715f4a20b0de3635e
renyiwu <renyiwu@gmail.com>
2017-09-27 09:49:47 -0400

    second version

--- RNA-seq_general_hisat2.sh
+++ RNA-seq_general_hisat2.sh
@@ -1,4 +1,5 @@
-#
+#RNAs-seq workflow. R Wu Sept 2017
+#Hisat2 -> picard -> feartureCounts
 #########################
 #This version uses Hisat2
 #########################
@@ -36,3 +37,22 @@ featureCounts --primary -T 8 -a /path/to/genes.gtf -o featurecounts.results.csv 
 ###### one line command for all:
 
 rename 's/_S.*gz/.fastq.gz/' *.fastq.gz && for i in *.fastq.gz; do hisat2 -p 6 -x ~/genome/mm10/genome -U $i | samtools view -bh -o ${i%.fastq.gz}.bam -; samtools sort -@ 6 -o ${i%.fastq.gz}.sorted.bam ${i%.fastq.gz}.bam; rm ${i%.fastq.gz}.bam; samtools rmdup -s ${i%.fastq.gz}.sorted.bam ${i%.fastq.gz}.rmdup.bam; rm ${i%.fastq.gz}.sorted.bam; done && featureCounts --primary -T 8 -a ~/genome/mm10/genes.gtf -o featurecounts.results.csv *dup.bam
+
+####################
+#   Version 2      #
+####################
+#
+#Rename (shorten) fastq file names if needed.
+rename 's/_S.*gz/.fastq.gz/' *.fastq.gz
+#eg, C75_S1_R1_001_.fastq.gz to C75.fastq.gz
+#
+#concatnate. eg, RW041 and RW042.
+zcat RW04?.fastq.gz | gzip >RW04.fastq.gz
+
+#Count reads
+for i in RW??.fastq.gz; do zcat $i | echo $((`wc -l`/4)); done
+
+#run
+for i in *.fastq.gz; do hisat2 -p 6 -x ~/genome/mm10/genome -U $i | samtools sort -@ 6 --output-fmt SAM | samtools rmdup -s --output-fmt BAM - ${i%.fastq.gz}.rmdup.bam; done && featureCounts --primary -T 8 -a ~/genome/mm10/genes.gtf -o featurecounts.results.csv *dup.bam
+
+#
