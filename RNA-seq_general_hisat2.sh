26f687dd6047d2f704e6c59dfe1c06dc6a686703
renyiwu <renyiwu@gmail.com>
2017-09-27 10:01:18 -0400

    new

--- RNA-seq_general_hisat2.sh
+++ RNA-seq_general_hisat2.sh
@@ -55,4 +55,61 @@ for i in RW??.fastq.gz; do zcat $i | echo $((`wc -l`/4)); done
 #run
 for i in *.fastq.gz; do hisat2 -p 6 -x ~/genome/mm10/genome -U $i | samtools sort -@ 6 --output-fmt SAM | samtools rmdup -s --output-fmt BAM - ${i%.fastq.gz}.rmdup.bam; done && featureCounts --primary -T 8 -a ~/genome/mm10/genes.gtf -o featurecounts.results.csv *dup.bam
 
+#########################################################
+#Further analyses with counting results by featureCounts:
+#Run in R
+R
+library(DESeq2)
+library(data.table)
+library("gplots")
+# load matrix of read counts
+df <- read.table("data/shan_rna/RW_all_primary.dedup.csv", header = T, row.names = "Geneid")
+df <- df[,-(1:4)]
+colnames(df) <- c("length", paste("RW", 1:7, sep = ""))
+df_7 <- df[-1]
+# create sample matrix
+mat <- matrix(paste("S",1:7, sep = ""), nrow = 7)
+colnames(mat) <- "condition"
+mat
+rownames(mat) <- colnames(df[-1])
+mat_7 <- mat
+dds <- DESeqDataSetFromMatrix(countData=df_7, colData=mat_7, design= ~ condition)
+mcols(dds)$basepairs <- df$length
+dds_7 <- DESeq(dds)
+
+write.table(fpkm(dds_7), file = "data/shan_rna/RW_all.fpkm.csv", sep = "\t", quote = T, col.names = NA)
+
 #
+fpkm_7 <- fpkm(dds_7)
+fpkm_71 <- fpkm_7+1 #Add pseudo 1 to all values to avoid log10(0) error.
+fpkm_72 <- log10(fpkm_7+1) #log10 transformation
+mat_f <- fpkm_72[!rowSums(fpkm_72 <1),] #Only keep rows which have values greater than or equal to 2 (in all groups/columns)
+write.table(log10(fpkm(dds_7)+1), file = "data/shan_rna/RW_all_log10_fpkm.csv", sep = "\t", quote = T, col.names = NA)
+write.table(mat_f, file = "data/shan_rna/RW_all_log10_fpkm_gt1.csv", sep = "\t", quote = T, col.names = NA)
+
+#tmp <- read.table(file = "data/shan_rna/RW_all_log10_fpkm_gt11.csv", sep = "\t", header = T, row.names = "X")
+#heatmap
+lmat <- rbind(c(4,1),c(3,1),c(2,1))
+lmat
+dev.off()
+heatmap.2(mat_f,col=redgreen(255),dendrogram = "none",Colv = FALSE,Rowv = FALSE, 
+          trace = "none",
+          density.info = "none", key = FALSE, labRow = NA, labCol = NA,
+          lmat= lmat,  lwid = c(0,4), lhei = c(4,4,4))
+heatmap.2(mat,col=bluered(255),dendrogram = "none",Colv = FALSE, 
+          density.info = "none", key = FALSE, labCol = NA,
+          trace = "none",
+          lmat= lmat,  lwid = c(0,4), lhei = c(4,4,4))
+heatmap.2(mat_f)
+
+
+dds$condition <- relevel(dds$condition, ref="S1")
+# filter for genes with at least one count in at least two samples:
+dds <- dds[ rowSums(counts(dds) >= 1) >= 2, ]  
+#Run deseq
+dds_7 <- DESeq(dds)
+# pairwise comparisons
+res1 <- results(dds_7,contrast=c("condition", "S2", "S1"))
+#write out
+write.table(res1, file="data/shan_rna/S2_S1.csv", sep="\t", quote=T, col.names=NA)
+
