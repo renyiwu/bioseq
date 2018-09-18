e56d44459683fe7746daf0b76c7ee75c907c9fa7
renyi <renyi@SOP-1482>
2018-09-18 18:07:10 -0400

    2nd

--- count_bam.sh
+++ count_bam.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-
-
-date >> bam_file_reads.txt
+# R Wu Sep 2018
+date
+SECONDS=0
+date > bam_file_reads.txt
 for i in $@
 do
-samtools view $i | echo -n $(($(wc -l)/4)) >> bam_file_reads.txt
+samtools view $i | echo -n $(wc -l) >> bam_file_reads.txt
 echo -e "\t$i" >> bam_file_reads.txt
 done
 echo -e "\n\n" >> bam_file_reads.txt
+date
+echo
+echo "Finished in $SECONDS seconds."
